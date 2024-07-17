acd8c3f71bf615c0072ba5cf9fab658e4532f75b
SOMNATH <somnathnikam2076@gmail.com>
2024-07-17 23:53:43 +0530

    Stylish Bottom Navigation Achieved through Chip

--- app/src/main/java/com/example/stylishnavigation/MainActivity.kt
+++ app/src/main/java/com/example/stylishnavigation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.stylishnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.stylishnavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,23 @@ class MainActivity : AppCompatActivity() {
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
+        setupbar()
+
+    }
+
+    private fun setupbar() {
+        binding.chipnav.setOnItemSelectedListener {
+            when(it){
+                R.id.message -> binding.textView.text="Chat"
+                R.id.person -> binding.textView.text="person"
+                R.id.nearMe ->{ binding.textView.text="nearme"
+                binding.chipnav.showBadge(R.id.nearMe)
+                }
+                R.id.settings -> binding.textView.text="Settins"
+
+            }
+        }
     }
 }
