bc3917929aedf9460af942fcdfa27ae9ed8f8a23
SOMNATH <somnathnikam2076@gmail.com>
2024-07-18 10:17:53 +0530

    Changes in bottom Na Color

--- app/src/main/java/com/example/stylishnavigation/MainActivity.kt
+++ app/src/main/java/com/example/stylishnavigation/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.stylishnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatDelegate
 import com.example.stylishnavigation.databinding.ActivityMainBinding
 
@@ -16,19 +18,30 @@ class MainActivity : AppCompatActivity() {
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
-        setupbar()
+        setupNavbar()
 
     }
 
-    private fun setupbar() {
+    private  fun setupNavbar() {
         binding.chipnav.setOnItemSelectedListener {
             when(it){
-                R.id.message -> binding.textView.text="Chat"
-                R.id.person -> binding.textView.text="person"
-                R.id.nearMe ->{ binding.textView.text="nearme"
-                binding.chipnav.showBadge(R.id.nearMe)
+                R.id.message -> {
+                    binding.textView.text = "Chat"
+                    Toast.makeText(this,"Chat",Toast.LENGTH_LONG).show()
                 }
-                R.id.settings -> binding.textView.text="Settins"
+                R.id.person -> {
+                    binding.textView.text = "Person"
+                    Toast.makeText(this,"Person",Toast.LENGTH_LONG).show()
+                }
+                R.id.nearMe ->{ binding.textView.text="Near me"
+                binding.chipnav.showBadge(R.id.nearMe)
+                    Toast.makeText(this,"Near Me",Toast.LENGTH_LONG).show()
+                }
+                R.id.settings -> {
+                    binding.textView.text="Settings"
+                    binding.chipnav.dismissBadge(R.id.settings)
+                    Toast.makeText(this,"Settings",Toast.LENGTH_LONG).show()
+                }
 
             }
         }
